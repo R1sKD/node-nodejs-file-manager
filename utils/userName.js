c489937fe41574ef3f7e0305e9bccd2764b4c652
Denis Prokhorenko <denis07011997q@gmail.com>
2022-06-11 09:51:38 +0300

    fix: userName func

--- utils/userName.js
+++ utils/userName.js
@@ -1,6 +1,6 @@
-export const userName = getUserName();
-
 const getUserName = () => {
   const userName = process.argv.slice(2).join('').split('=')[1];
   return userName;
 };
+
+export const userName = getUserName();
