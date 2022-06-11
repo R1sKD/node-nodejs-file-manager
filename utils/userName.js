1572bafadacdedf46b65928fc0d8750ba1c3fc06
Denis Prokhorenko <denis07011997q@gmail.com>
2022-06-11 11:37:07 +0300

    refactor: get user name

--- utils/userName.js
+++ utils/userName.js
@@ -1,5 +1,7 @@
 const getUserName = () => {
-  const userName = process.argv.slice(2).join('').split('=')[1];
+  const args = process.argv.slice(2).join('').split('=');
+  const userNameIndex = args.findIndex((arg) => arg === '--username');
+  const userName = userNameIndex !== -1 ? args[userNameIndex + 1] : 'Stranger';
   return userName;
 };
 
