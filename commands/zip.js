6de65a4b4ae62a698bf7d94051718141054f9268
Denis Prokhorenko <denis07011997q@gmail.com>
2022-06-11 11:24:09 +0300

    fix: zip funcs

--- commands/zip.js
+++ commands/zip.js
@@ -3,10 +3,14 @@ import { pipeline } from 'stream';
 import { createReadStream, createWriteStream } from 'fs';
 import { promisify } from 'util';
 import { showFailedMessage } from '../utils/messages.js';
+import path from 'path';
 
 const pipe = promisify(pipeline);
 
 export const compress = async (input, output) => {
+  input = path.resolve(input);
+  const fileName = path.basename(input);
+  output = path.join(path.resolve(output), `${fileName}.br`);
   const brotliZip = createBrotliCompress();
   const source = createReadStream(input);
   const destination = createWriteStream(output);
@@ -16,10 +20,14 @@ export const compress = async (input, output) => {
 };
 
 export const decompress = async (input, output) => {
+  input = path.resolve(input);
+  const fileName = path.basename(input).split('.').slice(0, -1).join('.');
+  output = path.join(path.resolve(output), fileName);
   const brotliUnzip = createBrotliDecompress();
   const source = createReadStream(input);
   const destination = createWriteStream(output);
   await pipe(source, brotliUnzip, destination).catch((err) => {
+    console.log(err);
     showFailedMessage();
   });
 };
